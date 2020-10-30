d0876dd2c3750acf6f6f1e6d6a12c88869ff5110
José Duque <cj_duque@hotmail.com>
2020-10-30 09:46:47 -0300

    adding catogories module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CategoriesModule } from './pages/categories/categories.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CategoriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
