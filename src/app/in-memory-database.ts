3e0e079d72721b2edbb5185fd396776b17337d23
José Duque <cj_duque@hotmail.com>
2020-11-01 13:56:22 -0300

    adding cotegory model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,15 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model'
+
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
-    const categories = [
+    const categories: Category[] = [
       {id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa'},
       {id: 2, name: 'Saude', description: 'Planos de saúde e remedios'},
       {id: 3, name: 'Lazer', description: 'cinema, parques, praia, etc'},
       {id: 4, name: 'Salário', description: 'Recebimento de salário'},
-      {id: 5, name: 'Freelas', description: 'trabalho como freelance'},
+      {id: 5, name: 'Freelas', description: 'trabalho como freelance'}
     ]
 
     return {categories}
