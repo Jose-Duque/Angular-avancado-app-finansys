75bbf1477f4a096c3c9aa6a35417001c6ef99eb8
José Duque <cj_duque@hotmail.com>
2020-11-09 15:48:13 -0300

    adding association between Entry and Category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { map, catchError, flatMap } from "rxjs/operators";
 
+import { CategoryService } from "../../categories/shared/category.service";
 import { Entry } from "../shared/entry.model";
 
 
@@ -14,7 +15,7 @@ export class EntryService {
 
   private apiPath: string = 'api/entries'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -32,18 +33,32 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handlError),
-      map(this.jsonDataToEntry)
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handlError),
+          map(this.jsonDataToEntry)
+        )
+      })
     )
   }
 
+
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`
-    return this.http.put(url, entry).pipe(
-      catchError(this.handlError),
-      map(() => entry)
+    return this,this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handlError),
+          map(() => entry)
+        )
+      })
     )
+
   }
 
   delete(id: number): Observable<Entry> {
@@ -73,4 +88,6 @@ export class EntryService {
     console.log('ERRO NA REQUISÃO -->', error);
     return throwError(error);
   }
+
+
 }
